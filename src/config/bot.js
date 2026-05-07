0ff945215484bc2d23766ee2fee7dc4319df7043
yurpawsies <gretilevi4ever@gmail.com>
2026-05-07 21:21:32 +0200

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,7 +25,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "yurpawsies discord",
+        name: "/numbwrld's discord",
         // Activity type number (0 = Playing).
         type: 3, 
       },
